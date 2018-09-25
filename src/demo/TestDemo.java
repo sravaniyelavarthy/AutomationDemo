75b6636bcdf8cdd6dbf0433382c10c62bb287f15
sravaniyelavarthy <43559385+sravaniyelavarthy@users.noreply.github.com>
2018-09-25 12:19:25 +0530

    Deleted one file

--- src/demo/TestDemo.java
+++ src/demo/TestDemo.java
@@ -14,8 +14,8 @@ public class TestDemo {
 	{
 		FileInputStream fis=new FileInputStream("./data/Book1.xlsx");
 		Workbook w = WorkbookFactory.create(fis);
-		String value = w.getSheet("Sheet1").getRow(1).getCell(1).toString();
-		System.out.println(value);
+		String value2 = w.getSheet("Sheet1").getRow(1).getCell(1).toString();
+		System.out.println(value2);
 		
 		
 	}
